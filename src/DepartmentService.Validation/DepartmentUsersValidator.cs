e37b6f93dd10d81212d0099f5cf4a5b8a2ffbf41
sparrowofnight@gmail.com <sparrowofnight@gmail.com>
2022-04-30 21:49:17 +0300

    CreateDepartmentRequestValidator changed

--- src/DepartmentService.Validation/DepartmentUsersValidator.cs
+++ src/DepartmentService.Validation/DepartmentUsersValidator.cs
@@ -4,33 +4,22 @@ using System.Linq;
 using FluentValidation;
 using LT.DigitalOffice.DepartmentService.Broker.Requests.Interfaces;
 using LT.DigitalOffice.DepartmentService.Validation.Interfaces;
-using LT.DigitalOffice.Models.Broker.Common;
-using MassTransit;
-using Microsoft.Extensions.Logging;
 
 namespace LT.DigitalOffice.DepartmentService.Validation
 {
   public class DepartmentUsersValidator : AbstractValidator<List<Guid>>, IDepartmentUsersValidator
   {
-    private readonly IRequestClient<ICheckUsersExistence> _rcCheckUsersExistence;
-    private readonly ILogger<DepartmentUsersValidator> _logger;
-
     public DepartmentUsersValidator(
-      IRequestClient<ICheckUsersExistence> rcCheckUsersExistence,
-      ILogger<DepartmentUsersValidator> logger)
+      IUserService userservice)
     {
-      _rcCheckUsersExistence = rcCheckUsersExistence;
-      _logger = logger;
-      IUserService _userservice = default;
-
       RuleForEach(users => users)
         .Cascade(CascadeMode.Stop)
-        .NotEmpty().WithMessage("Users ids must not be empty.")
+        .NotEmpty().WithMessage("Wrong type of user Id.")
         .ChildRules(users =>
           RuleFor(users => users)
             .Must(ids => ids.Distinct().Count() == ids.Count())
             .WithMessage("User cannot be added to the deaprtment twice.")
-            .MustAsync(async (ids, _) => ids.Count == (await _userservice.CheckUsersExistenceAsync(ids)).Count)
+            .MustAsync(async (ids, _) => ids.Count == (await userservice.CheckUsersExistenceAsync(ids)).Count)
             .WithMessage("Some users does not exist."));
     }
   }
