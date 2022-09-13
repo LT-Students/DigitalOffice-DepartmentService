7a82a9cfdcf84f66d6b4333ab7ee79743ffac27d
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-09-13 17:16:17 +0300

    Feature/require host healt check (#59)
    * add host requirement
    
    * add hc to skipped
    
    * change url
    
    * change health check url
    
    * add check for tests

--- src/DepartmentService.Business/Department/GetDepartmentCommand.cs
+++ src/DepartmentService.Business/Department/GetDepartmentCommand.cs
@@ -9,6 +9,8 @@ using LT.DigitalOffice.DepartmentService.Models.Dto.Requests.Department.Filters;
 using LT.DigitalOffice.DepartmentService.Models.Dto.Responses;
 using LT.DigitalOffice.Kernel.Helpers.Interfaces;
 using LT.DigitalOffice.Kernel.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace LT.DigitalOffice.DepartmentService.Business.Department
 {
@@ -16,20 +18,27 @@ namespace LT.DigitalOffice.DepartmentService.Business.Department
   {
     private readonly IDepartmentRepository _departmentRepository;
     private readonly IDepartmentResponseMapper _departmentResponseMapper;
+    private readonly ILogger<GetDepartmentCommand> _logger;
     private readonly IResponseCreator _responseCreator;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
     public GetDepartmentCommand(
       IDepartmentRepository departmentRepository,
       IDepartmentResponseMapper departmentResponseMapper,
-      IResponseCreator responseCreator)
+      ILogger<GetDepartmentCommand> logger,
+      IResponseCreator responseCreator,
+      IHttpContextAccessor httpContextAccessor)
     {
       _departmentRepository = departmentRepository;
       _departmentResponseMapper = departmentResponseMapper;
+      _logger = logger;
       _responseCreator = responseCreator;
+      _httpContextAccessor = httpContextAccessor;
     }
 
     public async Task<OperationResultResponse<DepartmentResponse>> ExecuteAsync(GetDepartmentFilter filter, CancellationToken cancellationToken = default)
     {
+      _logger.LogInformation($"<--------- REMOTE HOST IS: {_httpContextAccessor.HttpContext.Connection.RemoteIpAddress} ----------->");
       DbDepartment dbDepartment = await _departmentRepository.GetAsync(filter, cancellationToken);
 
       if (dbDepartment is null)
